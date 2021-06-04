da4cf3ef9a092fb6ae4341d857056b5d9c8213a4
tanto untung <untung2k3@yahoo.com>
2021-06-04 09:30:49 +0200

    next is wednesday lecture

--- src/api/peopleApi.js
+++ src/api/peopleApi.js
@@ -1,11 +1,12 @@
 
 
 export default function getPeople() {
-    return fetch('https://localhost:44348/api/React')
+    return fetch('https://localhost:44304/api/React')
+    //to call the table, must activate backend too
         .then(data => data.json());
 }
 
 export function getPersonById(id) {
-    return fetch('https://localhost:44348/api/React/' + id)
+    return fetch('https://localhost:44304/api/React/' + id)
         .then(data => data.json());
 }
